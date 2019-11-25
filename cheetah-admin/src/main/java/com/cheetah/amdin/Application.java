88e10e594d38ae63081e12ea980c7bf3956394a6
cuizhijia <cuizhijia@gomefinance.com.cn>
2019-11-25 18:50:17 +0800

    首次提交

---
+++ cheetah-admin/src/main/java/com/cheetah/amdin/Application.java
@@ -0,0 +1,22 @@
+package com.cheetah.amdin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @GetMapping("/")
+    public String hi() throws InterruptedException {
+        return "hello word";
+    }
+
+}
---
+++ cheetah-design/src/main/java/com/cheetah/design/code/aqs/AbstractQueuedSyn.java
@@ -0,0 +1,13 @@
+package com.cheetah.design.code.aqs;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class AbstractQueuedSyn {
+
+    abstract static class Sync extends AbstractQueuedSynchronizer {
+
+
+    }
+
+}
